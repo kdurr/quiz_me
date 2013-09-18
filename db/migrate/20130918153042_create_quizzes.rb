c0a2857707014bfe65ce6af1bfc8ee7572c91103
kdurr <kristina.l.durr@gmail.com>
2013-09-18 11:33:13 -0400

    created Quiz table

---
+++ db/migrate/20130918153042_create_quizzes.rb
@@ -0,0 +1,12 @@
+class CreateQuizzes < ActiveRecord::Migration
+  def change
+    create_table :quizzes do |t|
+      t.string :title, null:false
+      t.integer :age_rating
+      t.boolean :private, default:false
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
