7d2f05160b28c04eeb6ab9bc586b750e29a5572d
kdurr <kristina.l.durr@gmail.com>
2013-10-15 18:43:53 -0400

    add callback for submission model

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -6,4 +6,23 @@ class Submission < ActiveRecord::Base
   has_many :answers,
     inverse_of: :submission
   accepts_nested_attributes_for :answers
+
+  before_save :user_score
+
+  def user_score
+    @answers = self.answers
+    count = @answers.length
+    initial_result = 0
+    @answers.each do |a|
+      if a.question_choice.correct == true
+        initial_result += 1
+      end
+    end
+    if initial_result != count
+      calculated_result = ((count - initial_result).to_f/count.to_f)*100.0
+    else
+      calculated_result = 100
+    end
+    self.result = calculated_result
+  end
 end
--- spec/features/user_searchs_for_quiz_spec.rb
+++ spec/features/user_searchs_for_quiz_spec.rb
@@ -6,6 +6,14 @@ feature 'user searches for a quiz', %Q{
   To gauge what quiz I want to take
 } do
   
-  scenario 'existing quiz will appear when searched'
+  scenario 'existing quiz will appear when searched' do
+    quiz = FactoryGirl.create(:quiz)
+    visit quizzes_path
+    fill_in 'q_title_cont', with: quiz.title
+    click_on 'Search'
+
+    save_and_open_page
+    expect(page).to have_content(quiz.title)
+  end
 
 end
